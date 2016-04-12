a8b1b3a9b948cd93b7ae7f14379f77808d962163
Jonas Ådahl <jadahl@gmail.com>
2016-04-12 17:15:04 +0800

    Unset the allowed gdk backends

--- xdg-app-gtk.c
+++ xdg-app-gtk.c
@@ -218,7 +218,10 @@ handle_file_chooser_open_file (XdgAppDesktopFileChooserBackend *object,
     }
 
   if (!display)
-    gdk_display_get_default ();
+    {
+      gdk_set_allowed_backends (NULL);
+      gdk_display_get_default ();
+    }
 
   screen = gdk_display_get_default_screen (display);
 
